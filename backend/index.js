2c440ff5acabb617bf2f5803e18b615b5927b6c0
AmareGatie <amaregatie2006@gmail.com>
2024-06-26 11:05:53 +0300

    done

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ mongoose
 
 // Middleware for CORS and JSON parsing
 const allowedOrigins = [
-  "https://trips-travel.vercel.app",
+  "https://trips-travel-nu.vercel.app",
   "http://localhost:5173",
   "http://localhost:3050"
 ];
